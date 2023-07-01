4edcd29049bd320aac632209998f926214339898
cc66 <cc66@DESKTOP-UR4JC28>
2023-07-01 18:34:01 -0500

    edit_categorias aniadido esperando revision

--- src/main/java/com/mycompany/sistema_biblioteca/Edit_Categoria.java
+++ src/main/java/com/mycompany/sistema_biblioteca/Edit_Categoria.java
@@ -4,10 +4,14 @@
  */
 package com.mycompany.sistema_biblioteca;
 
+import Controllers.CategoriaController;
 import Model.Usuario;
 import Services.RusuarioRepository;
 import java.util.List;
 import Controllers.UsuarioController;
+import Model.Categorias_US;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
 /**
  *
  * @author santi
@@ -17,11 +21,26 @@ public class Edit_Categoria extends javax.swing.JInternalFrame {
     /**
      * Creates new form Edit_Categoria
      */
-    UsuarioController UsController = new UsuarioController();
+    
+    private DefaultComboBoxModel Cbox = new DefaultComboBoxModel();
+    UsuarioController uController = new UsuarioController();
+    CategoriaController CatController = new CategoriaController();
+    
+    
+    
     public Edit_Categoria() {
         initComponents();
+        CargarCategorias();
     }
 
+    public void CargarCategorias(){
+        List<Categorias_US> lst = CatController.GetAllCategroiaController();
+        for(Categorias_US item:lst){
+            this.CboxCategoria.addItem(  item.getNombre_Categoria());
+            this.CbxNuevaCategoria.addItem(  item.getNombre_Categoria());
+        }
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -32,42 +51,39 @@ public class Edit_Categoria extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
-        CBXCats = new javax.swing.JComboBox<>();
+        CbxNuevaCategoria = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
-        txtCodigo = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        TxtEstadoActual = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         Aceptar = new javax.swing.JButton();
-        Cancel = new javax.swing.JButton();
         BtnBuscar = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
-        UserNome = new javax.swing.JTextField();
+        txtNombre = new javax.swing.JTextField();
+        txtCodigo = new javax.swing.JTextField();
+        CboxCategoria = new javax.swing.JComboBox<>();
 
         jButton1.setText("jButton1");
 
         setTitle("Editar Categoria");
 
-        jLabel1.setText("Id Usuario");
-
-        txtCodigo.addActionListener(new java.awt.event.ActionListener() {
+        CbxNuevaCategoria.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtCodigoActionPerformed(evt);
+                CbxNuevaCategoriaActionPerformed(evt);
             }
         });
+
+        jLabel1.setText("Id Usuario");
 
         jLabel2.setText("Nueva Categoria");
 
         jLabel3.setText("Categoria Actual del Usuario");
 
-        Aceptar.setText("Aceptar");
+        Aceptar.setText("Actualizar");
         Aceptar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 AceptarActionPerformed(evt);
             }
         });
-
-        Cancel.setText("Cancelar");
 
         BtnBuscar.setText("Buscar");
         BtnBuscar.addActionListener(new java.awt.event.ActionListener() {
@@ -78,46 +94,53 @@ public class Edit_Categoria extends javax.swing.JInternalFrame {
 
         jLabel4.setText("Nombre del Usuario");
 
+        txtNombre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNombreActionPerformed(evt);
+            }
+        });
+
+        txtCodigo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtCodigoActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(27, 27, 27)
+                        .addGap(0, 42, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(CboxCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addGap(18, 18, 18)
+                                .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(27, 47, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addGap(29, 29, 29)
-                                .addComponent(CBXCats, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(CbxNuevaCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addGap(16, 16, 16)
-                                        .addComponent(jLabel4)))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel1)
+                                .addGap(31, 31, 31)
+                                .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(12, 12, 12)
                                 .addComponent(BtnBuscar))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(72, 72, 72)
-                        .addComponent(Aceptar)
-                        .addGap(71, 71, 71)
-                        .addComponent(Cancel)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 25, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(18, 18, 18)
-                        .addComponent(TxtEstadoActual, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(101, 101, 101))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(UserNome, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(35, 35, 35))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(92, 92, 92)
+                        .addComponent(Aceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap(12, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -125,24 +148,22 @@ public class Edit_Categoria extends javax.swing.JInternalFrame {
                 .addGap(36, 36, 36)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(BtnBuscar))
+                    .addComponent(BtnBuscar)
+                    .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(UserNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TxtEstadoActual, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
+                    .addComponent(jLabel3)
+                    .addComponent(CboxCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(CBXCats, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(CbxNuevaCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Aceptar)
-                    .addComponent(Cancel))
+                .addComponent(Aceptar)
                 .addGap(56, 56, 56))
         );
 
@@ -151,34 +172,65 @@ public class Edit_Categoria extends javax.swing.JInternalFrame {
 
     private void AceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AceptarActionPerformed
         // TODO add your handling code here:
+        Usuario old = uController.BuscarCodigoController(Integer.parseInt(this.txtCodigo.getText()));
+        Categorias_US objCats = CatController.GetCategoriaXNombre(GetSelectedCatNueva());
+        Usuario objUS = new Usuario();
+        objUS.setCodigo(Integer.parseInt(this.txtCodigo.getText()));
+        objUS.setNombre(this.txtNombre.getText());
+        objUS.setCategoria(objCats.getNombre_Categoria());
+        objUS.setContraseña(old.getContraseña());
+        objUS.setEmail(old.getEmail());
+        objUS.setId_Cat(objCats.getId_Categoria());
+        
+        uController.ActualizarUsController(objUS);
+        JOptionPane.showMessageDialog(this, "Registro Actualizado!!");
     }//GEN-LAST:event_AceptarActionPerformed
+
+    public String GetSelectedCat(){
+        return CboxCategoria.getSelectedItem().toString();
+    }
+    
+    public String GetSelectedCatNueva(){
+        return CbxNuevaCategoria.getSelectedItem().toString();
+    }
+    
+    private void BtnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnBuscarActionPerformed
+        // TODO add your handling code here:
+        Usuario objUs = uController.BuscarCodigoController(Integer.parseInt(this.txtCodigo.getText()));
+        
+            this.txtNombre.setText(objUs.getNombre());
+           // System.out.println("error 1");
+            this.CboxCategoria.setSelectedItem(objUs.getCategoria());
+           // System.out.println("error 2");
+            
+            txtCodigo.setEditable(false);
+            
+    }//GEN-LAST:event_BtnBuscarActionPerformed
+
+    private void txtNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNombreActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNombreActionPerformed
+
+    private void CbxNuevaCategoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CbxNuevaCategoriaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_CbxNuevaCategoriaActionPerformed
 
     private void txtCodigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCodigoActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtCodigoActionPerformed
 
-    private void BtnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnBuscarActionPerformed
-        // TODO add your handling code here:
-        Usuario lst = UsController.BuscarCodigoController(Integer.parseInt(this.txtCodigo.getText()));
-        for(Usuario x:lst){
-            this.UserNome.setText(x.getNombre());
-            this.TxtEstadoActual.setText(x.getCategoria());
-        }
-    }//GEN-LAST:event_BtnBuscarActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Aceptar;
     private javax.swing.JButton BtnBuscar;
-    private javax.swing.JComboBox<String> CBXCats;
-    private javax.swing.JButton Cancel;
-    private javax.swing.JTextField TxtEstadoActual;
-    private javax.swing.JTextField UserNome;
+    private javax.swing.JComboBox<String> CboxCategoria;
+    private javax.swing.JComboBox<String> CbxNuevaCategoria;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JTextField txtCodigo;
+    private javax.swing.JTextField txtNombre;
     // End of variables declaration//GEN-END:variables
 }
